60b81992adc3c017645ed2d865c362a512f475a9
Niklas Grenningloh <51596575+base2code@users.noreply.github.com>
2022-07-26 11:01:23 +0200

    Create basic-iptables.sh

---
+++ basic-iptables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A OUTPUT -o lo -j ACCEPT
+sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
