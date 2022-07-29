7d106f78dea535942e94c9247075d23fcf9bdae9
Niklas Grenningloh <51596575+base2code@users.noreply.github.com>
2022-07-29 17:39:48 +0200

    Create install-lynix.sh

---
+++ install-lynix.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget -O - https://packages.cisofy.com/keys/cisofy-software-public.key | sudo apt-key add -
+echo "deb https://packages.cisofy.com/community/lynis/deb/ stable main" | sudo tee /etc/apt/sources.list.d/cisofy-lynis.list
+sudo apt-get -y update # Update the package list
+sudo apt install lynis -y # Install Lynis
+sudo lynis show version
