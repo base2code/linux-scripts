a68bbde01d03e0cc55b1d1fd2f0ee6f44b8ae865
Niklas Grenningloh <51596575+base2code@users.noreply.github.com>
2022-10-09 19:09:26 +0200

    Update install-certbot.sh

--- scripts/install-certbot.sh
+++ scripts/install-certbot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Install Certbot via snapd (over apt) and create symlink
 sudo apt install snapd -yqq
 sudo snap install core; sudo snap refresh core
 sudo snap install --classic certbot
