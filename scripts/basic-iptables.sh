0021cc0960806e58c30c6837e91ded5377b4fb5b
Niklas Grenningloh <51596575+base2code@users.noreply.github.com>
2022-10-09 19:08:36 +0200

    Update basic-iptables.sh

--- scripts/basic-iptables.sh
+++ scripts/basic-iptables.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#EXPERIMENTAL - basic iptables setup (rules)
 sudo iptables -A INPUT -i lo -j ACCEPT
 sudo iptables -A OUTPUT -o lo -j ACCEPT
 sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
