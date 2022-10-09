b82f301a1da5f1276af390677a4c2d81b6df0b7b
Niklas Grenningloh <51596575+base2code@users.noreply.github.com>
2022-10-09 19:11:13 +0200

    Update install-lynix.sh

--- scripts/install-lynix.sh
+++ scripts/install-lynix.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Install Lynix via apt (deb)
 wget -O - https://packages.cisofy.com/keys/cisofy-software-public.key | sudo apt-key add -
 echo "deb https://packages.cisofy.com/community/lynis/deb/ stable main" | sudo tee /etc/apt/sources.list.d/cisofy-lynis.list
 sudo apt-get -y update # Update the package list
