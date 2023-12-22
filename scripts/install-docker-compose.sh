cdf8dae9eaf816f69e38d9b2ab2ab7a63d831a26
Niklas Grenningloh <niklas@grenningloh.net>
2023-12-22 14:18:22 +0100

    Update docker-compose version

--- scripts/install-docker-compose.sh
+++ scripts/install-docker-compose.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Install x86_64 linux version of docker-compose
-sudo curl -sSL https://github.com/docker/compose/releases/download/v2.7.0/docker-compose-linux-x86_64 -o /usr/bin/docker-compose
+sudo curl -sSL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o /usr/bin/docker-compose
 sudo chmod +x /usr/bin/docker-compose
